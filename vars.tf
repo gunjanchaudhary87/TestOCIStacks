dc8bdda8484bb671a7ff866a0d8b75927993fff3
gunjanchaudhary87 <72584555+gunjanchaudhary87@users.noreply.github.com>
2023-08-14 16:43:23 +0530

    Add files via upload

--- vars.tf
+++ vars.tf
@@ -18,11 +18,7 @@ variable pltfm_compartment_name {
 	type = string
 }
 
-variable function_application_name {
-	type = string
-}
-
-variable streaming_function_name {
+variable stream_pool_id {
 	type = string
 }
 
@@ -85,30 +81,3 @@ variable stream_defined_tags {
     type = map
 }
 
-variable serviceconnector_display_name  {
-	type = string
-}
-
-variable serviceconnector_description  {
-	type = string
-}
-
-variable serviceconnector_state  {
-	type = string
-}
-
-variable serviceconnector_source_cursor_kind  {
-	type = string
-}
-
-variable source_kind {
-	type = string
-}
-
-variable target_kind {
-	type = string
-}
-
-variable serviceconnector_defined_tags {
-    type = map
-}
